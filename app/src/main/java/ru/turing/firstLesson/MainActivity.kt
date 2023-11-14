9c8382cb2655c458e4bbe9eb624d7855511970c3
ramgot <RamazanovOY@mpei.ru>
2023-11-14 23:58:47 +0300

    Value converter

--- app/src/main/java/ru/turing/firstLesson/MainActivity.kt
+++ app/src/main/java/ru/turing/firstLesson/MainActivity.kt
@@ -1,67 +1,58 @@
 package ru.turing.firstLesson
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
-import android.widget.Toast
-import org.w3c.dom.Text
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var radioButton: RadioButton
-    private lateinit var radioGroup: RadioGroup
-    private lateinit var textView: TextView
-    private lateinit var plainText: EditText
-    private lateinit var button: Button
-    var isTemperatureMode: Boolean = true
+    private lateinit var converterMods: RadioGroup
+    private lateinit var resultText: TextView
+    private lateinit var inputValueText: EditText
+    private lateinit var convertButton: Button
+    private var isTemperatureMode: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        plainText = findViewById<EditText>(R.id.text_before_converting)
-        textView = findViewById<TextView>(R.id.text_after_converting)
-        button = findViewById<Button>(R.id.button_convert)
-        radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+        inputValueText = findViewById(R.id.text_before_converting)
+        resultText = findViewById(R.id.text_after_converting)
+        convertButton = findViewById(R.id.button_convert)
+        converterMods = findViewById(R.id.radio_group)
 
 
-        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+        converterMods.setOnCheckedChangeListener { _, checkedId ->
             findViewById<RadioButton>(checkedId)?.apply {
                 if (isTemperatureMode) {
                     isTemperatureMode = !isTemperatureMode
-                    plainText.hint = "Введите конвертируемое расстояние, м"
-                    textView.text = "Результат будет тут"
+                    inputValueText.hint = "Введите конвертируемое расстояние, м"
+                    resultText.text = "Результат будет тут"
                 } else {
                     isTemperatureMode = !isTemperatureMode
-                    plainText.hint = "Введите конвертируемую температуру, °C"
-                    textView.text = "Результат будет тут"
+                    inputValueText.hint = "Введите конвертируемую температуру, °C"
+                    resultText.text = "Результат будет тут"
                 }
             }
         }
 
-        button.setOnClickListener{view ->
+        convertButton.setOnClickListener { view ->
             convert()
         }
     }
 
     private fun convert() {
-        textView.text = ""
-        try {
-            var input = plainText.text.toString().toDouble()
-            if (isTemperatureMode) {
-                input += 273
-            } else {
-                input /= 1000
-            }
-            textView.text = input.toString()
+        resultText.text = ""
+        var inputValue = inputValueText.text.toString().toDouble()
+        if (isTemperatureMode) {
+            inputValue += 273
+        } else {
+            inputValue /= 1000
         }
-        catch (e: Exception) {
-            textView.text = "Некорректный формат ввода!"
-        }
+        resultText.text = inputValue.toString()
     }
 }
 
